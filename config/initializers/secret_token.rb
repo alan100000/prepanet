00ad12f9d4f29f1d706345785ae978c37b2736ee
alan100000 <omgitsalan@gmail.com>
2011-02-17 17:14:41 -0600

    Se genero el proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prepanet::Application.config.secret_token = '0b02a0d59ec3fb0f05257a4a6404922d3d800f4657930d1ed70c6fca5c6a35951b008bd6f5a40d035d07b27f57b5326356cb03c0dc11f4bd0cfb38a885914091'
