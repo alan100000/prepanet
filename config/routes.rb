e6bccb0e3030be1aa658e09c55f2dc10620828d4
alan100000 <omgitsalan@gmail.com>
2011-02-17 18:32:33 -0600

    Modificado index, usuarios creados

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Prepanet::Application.routes.draw do
+  get "home/index"
+
+  devise_for :users
+
+   root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,6 @@ Prepanet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
