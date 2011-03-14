f3c1e1acd42722cbee35ccf4e953f3a3840779a0
Guayo <higgg.1989@gmail.com>
2011-03-14 03:54:25 -0600

    AYUDAAA

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,31 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    def index
+        super
+    end
+    
+    def show
+        super
+    end
+    
+    def new
+        super
+    end
+    
+    def edit
+        super
+    end
+    
+    def create
+        super
+    end
+    
+    def update
+        super
+    end
+    
+    def destroy
+        super
+    end
+
+end
