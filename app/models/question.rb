10283d8f7f288c0ef41ed005acb0fcbcf82c23ad
esalinas <eduardosalinas@hotmail.com>
2011-03-17 20:35:42 -0600

    le agregue la cosa de hace rpreguntas con sus respectivas respuetas

---
+++ app/models/question.rb
@@ -0,0 +1,4 @@
+class Question < ActiveRecord::Base
+  has_many :answers, :dependent => :destroy
+  accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+end
---
+++ db/migrate/20110318012554_create_answers.rb
@@ -0,0 +1,15 @@
+class CreateAnswers < ActiveRecord::Migration
+  def self.up
+    create_table :answers do |t|
+      t.integer :question_id
+      t.string :content
+      t.integer :option
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :answers
+  end
+end
